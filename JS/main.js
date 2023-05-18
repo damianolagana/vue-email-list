947fb3b7dcaec51ddb3b1c0fac9deb92b2c3ae58
damianolagana <damianolagana@gmail.com>
2023-05-18 10:06:42 +0200

    get email list fixed

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,34 @@
 const { createApp } = Vue
 
+const ul = document.getElementById("emailList");
+
 createApp({
     data() {
         return {
             chiave: "valore",
             emailPath: ' https://flynn.boolean.careers/exercises/api/random/mail',
+            mailList: [] ,
         }
     },
     methods: {
-        metodo() {
-            console.log("metodo che stampa un dato: ", this.chiave);
-        }
+  
     },
     mounted() {
         console.log("Mounted iniziato");
 
-        axios.get( this.emailPath ).then((risposta) => {
-            // debugger;
-            this.risposta = risposta.data.response;
-            console.log(risposta);
-        })  
+        for (let index = 1; index <= 10; index++) {
+            axios.get( this.emailPath ).then((risposta) => {
+            
+                this.mailList.push(risposta.data.response);
+                
+            })  
+            
+            
+        }
+
+      
 
         console.log("Mounted terminato");
+       
     }
 }).mount('#app')
